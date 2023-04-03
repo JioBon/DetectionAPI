68f3e125126f13e5a0d978d59150da321ef36c08
JJBonifacio <jcbonifacio.simplevia@gmail.com>
2023-04-04 00:40:06 +0800

    Add post method that takes image and cropname

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -6,22 +6,28 @@ app = FastAPI()
 class Msg(BaseModel):
     msg: str
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+@app.post("/baseimg/")
+async def create_upload_file(file: UploadFile = File(...), crop: str = Form(...)):
+    return {"filename": file.filename, "crop": crop}
 
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
+
+# ==============================================================================================================
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World. Welcome to FastAPI!"}
 
 
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+# @app.get("/path")
+# async def demo_get():
+#     return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+
+
+# @app.post("/path")
+# async def demo_post(inp: Msg):
+#     return {"message": inp.msg.upper()}
+
+
+# @app.get("/path/{path_id}")
+# async def demo_get_path_id(path_id: int):
+#     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
